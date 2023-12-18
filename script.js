ed031327ecfb53ae0da3bbd57f6c14683b52374b
Xiaochengzi <yu19940914@qq.com>
2023-12-18 11:13:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function showRandomAnswer() {
-    const answerElement = document.getElementById("answer");
-    // 访问数组中随机对象的 "message" 属性
-    const randomIndex = Math.floor(Math.random() * answers.length);
-    const answerText = answers[randomIndex].message;  // 获取随机答案的消息文本
-    answerElement.textContent = answerText;
-    answerElement.classList.remove('hidden'); // 确保答案是可见的
-    // 如果需要，可以在这里添加其他逻辑，比如改变按钮文本等
-}
+// 确保 DOM 完全加载后再绑定事件和操作元素
+document.addEventListener('DOMContentLoaded', function () {
+    function showRandomAnswer() {
+        const answerElement = document.getElementById("answer");
+        const randomIndex = Math.floor(Math.random() * answers.length);
+        const answer = answers[randomIndex].message;
+        answerElement.textContent = answer;
+        answerElement.classList.remove('hidden');
+    }
+
+    // 绑定按钮的点击事件
+    const answerButton = document.getElementById("answer-btn");
+    answerButton.addEventListener('click', showRandomAnswer);
+});
