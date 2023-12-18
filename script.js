6fb4c992bb397875f53112c2a6dfbf51e562db6d
Xiaochengzi <yu19940914@qq.com>
2023-12-18 11:11:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
-
 function showRandomAnswer() {
     const answerElement = document.getElementById("answer");
+    // 访问数组中随机对象的 "message" 属性
     const randomIndex = Math.floor(Math.random() * answers.length);
-    answerElement.textContent = answers[randomIndex];
-    answerElement.classList.toggle('hidden'); // 切换hidden类以显示或隐藏答案
+    const answerText = answers[randomIndex].message;  // 获取随机答案的消息文本
+    answerElement.textContent = answerText;
+    answerElement.classList.remove('hidden'); // 确保答案是可见的
+    // 如果需要，可以在这里添加其他逻辑，比如改变按钮文本等
 }
