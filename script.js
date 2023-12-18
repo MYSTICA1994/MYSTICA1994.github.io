dbe5ea622540a24d835d6d174d7ff3665152f055
Xiaochengzi <yu19940914@qq.com>
2023-12-18 12:10:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Ensure this matches the name of the event handler assigned in your HTML
-function showRandomMessage() {
-    // Get a random index based on the length of the answers array
-    const randomIndex = Math.floor(Math.random() * answers.length);
-    // Select the answer element where the message will be displayed
+document.addEventListener('DOMContentLoaded', function () {
+    const answerBtn = document.getElementById('answer-btn');
     const answerElement = document.getElementById('answer');
-    // Set the text content of this element to the message of the random answer
-    answerElement.textContent = answers[randomIndex].message;
-    // Remove the 'hidden' class to make the answer visible
-    answerElement.classList.remove('hidden');
-}
+
+    answerBtn.addEventListener('click', function() {
+        const randomIndex = Math.floor(Math.random() * answers.length);
+        answerElement.textContent = answers[randomIndex].message;
+        answerElement.classList.remove('hidden');
+    });
+});
