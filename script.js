87a73ec06ff8d2d8c490581b9b199a1e99e52126
Xiaochengzi <yu19940914@qq.com>
2023-12-18 11:36:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // 确保 DOM 完全加载后再绑定事件和操作元素
-document.addEventListener('DOMContentLoaded', function () {
-    function showRandomAnswer() {
-        const answerElement = document.getElementById("answer");
-        const randomIndex = Math.floor(Math.random() * answers.length);
-        const answer = answers[randomIndex].message;
-        answerElement.textContent = answer;
-        answerElement.classList.remove('hidden');
-    }
 
-    // 绑定按钮的点击事件
-    const answerButton = document.getElementById("answer-btn");
-    answerButton.addEventListener('click', showRandomAnswer);
+document.addEventListener('DOMContentLoaded', function () {
+    const answerButton = document.getElementById('answer-btn');
+    const resetButton = document.getElementById('reset-btn');
+    const card = document.getElementById('card');
+    const answerElement = document.getElementById('answer');
+
+    answerButton.addEventListener('click', function() {
+        const randomIndex = Math.floor(Math.random() * answers.length);
+        const answerText = answers[randomIndex].message;
+        answerElement.textContent = answerText;
+        card.classList.remove('hidden');
+        resetButton.classList.remove('hidden');
+    });
+
+    resetButton.addEventListener('click', function() {
+        card.classList.add('hidden');
+        resetButton.classList.add('hidden');
+        answerElement.textContent = '';
+    });
 });
