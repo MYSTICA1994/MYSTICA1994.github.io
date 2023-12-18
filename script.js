635913a921ffdfbf215af50183cee27bf3eadb2e
Xiaochengzi <yu19940914@qq.com>
2023-12-18 12:25:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
         answerElement.classList.remove('hidden');
     });
 });
+
+// Add DOMContentLoaded if not already present
+document.addEventListener('DOMContentLoaded', function () {
+    // Reference the reset button and add an event listener
+    const resetBtn = document.getElementById('reset-btn');
+    resetBtn.addEventListener('click', function() {
+        const answerElement = document.getElementById('answer');
+        answerElement.classList.add('hidden'); // Hide the answer
+        answerElement.textContent = ''; // Clear the answer text
+        resetBtn.classList.add('hidden'); // Hide the reset button
+    });
+});
+
+function showRandomAnswer() {
+    // ... existing code to show a random answer ...
+    const resetBtn = document.getElementById('reset-btn');
+    resetBtn.classList.remove('hidden'); // Show the reset button when an answer is shown
+}
