e07f993092fb4763be6d4cc702c5b11edc96d3fe
Xiaochengzi <yu19940914@qq.com>
2023-12-18 11:42:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-// 确保 DOM 完全加载后再绑定事件和操作元素
-
 document.addEventListener('DOMContentLoaded', function () {
-    const answerButton = document.getElementById('answer-btn');
-    const resetButton = document.getElementById('reset-btn');
-    const card = document.getElementById('card');
+    const answerBtn = document.getElementById('answer-btn');
     const answerElement = document.getElementById('answer');
 
-    answerButton.addEventListener('click', function() {
+    answerBtn.addEventListener('click', function() {
         const randomIndex = Math.floor(Math.random() * answers.length);
-        const answerText = answers[randomIndex].message;
-        answerElement.textContent = answerText;
-        card.classList.remove('hidden');
-        resetButton.classList.remove('hidden');
-    });
-
-    resetButton.addEventListener('click', function() {
-        card.classList.add('hidden');
-        resetButton.classList.add('hidden');
-        answerElement.textContent = '';
+        answerElement.textContent = answers[randomIndex].message;
+        answerElement.classList.toggle('hidden', false);
+        answerElement.classList.toggle('visible', true);
     });
 });
