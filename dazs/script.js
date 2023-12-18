b65ee8978fce3f3e69e57dbc80c85f3724a127a8
Xiaochengzi <yu19940914@qq.com>
2023-12-18 18:39:45 +0800

    u

--- dazs/script.js
+++ dazs/script.js
@@ -8,3 +8,18 @@ document.addEventListener('DOMContentLoaded', function () {
         answerElement.classList.remove('hidden');
     });
 });
+
+
+/*document.addEventListener('DOMContentLoaded', function () {
+    const answerBtn = document.getElementById('answer-btn');
+    const answerElement = document.getElementById('answer');
+    const answerCard = document.getElementById('answer-card');
+
+    answerBtn.addEventListener('click', function() {
+        const randomIndex = Math.floor(Math.random() * answers.length);
+        answerElement.textContent = answers[randomIndex].message;
+        answerCard.classList.remove('hidden');
+        answerCard.classList.add('card');
+    });
+});
+*/
