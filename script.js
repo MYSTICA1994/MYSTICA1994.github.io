27c53edcb454256b9c42e886b9bb90d030d4109f
Xiaochengzi <yu19940914@qq.com>
2023-12-18 10:46:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function showRandomMessage() {
     const chosenMessage = messages[randomIndex];
     document.getElementById("message").innerText = `序号${chosenMessage.number}，${chosenMessage.message}`;
 }
+
+// 显示或隐藏答案
+function toggleAnswer() {
+    var answer = document.getElementById("answer");
+    answer.classList.toggle("hidden");
+}
+
+// 隐藏答案
+function hideAnswer() {
+    var answer = document.getElementById("answer");
+    answer.classList.add("hidden");
+}
