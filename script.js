79a709aa1feafd196a6b78b07d3a461369db6b5e
Xiaochengzi <yu19940914@qq.com>
2023-12-18 11:45:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const answerBtn = document.getElementById('answer-btn');
+    const resetBtn = document.getElementById('reset-btn');
     const answerElement = document.getElementById('answer');
 
     answerBtn.addEventListener('click', function() {
         const randomIndex = Math.floor(Math.random() * answers.length);
         answerElement.textContent = answers[randomIndex].message;
-        answerElement.classList.toggle('hidden', false);
-        answerElement.classList.toggle('visible', true);
+        answerElement.classList.remove('hidden');
+    });
+
+    resetBtn.addEventListener('click', function() {
+        answerElement.textContent = '';
+        answerElement.classList.add('hidden');
     });
 });
