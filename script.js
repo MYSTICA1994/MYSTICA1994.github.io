03866c18dc7a2d4c5334414114b4272a2d43ffae
Xiaochengzi <yu19940914@qq.com>
2023-12-18 10:54:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function showRandomMessage() {
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    const chosenMessage = messages[randomIndex];
-    document.getElementById("message").innerText = `序号${chosenMessage.number}，${chosenMessage.message}`;
+// 假设您有一个包含答案的数组
+const answers = [
+    "命运赐予你的不只是这些答案。",
+    "深呼吸，相信直觉。",
+    // ... 其他答案
+];
+
+function showRandomAnswer() {
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    const answerElement = document.getElementById("answer");
+    answerElement.textContent = answers[randomIndex];
+    answerElement.classList.remove("hidden");
 }
 
-// 显示或隐藏答案
-function toggleAnswer() {
-    var answer = document.getElementById("answer");
-    answer.classList.toggle("hidden");
-}
-
-// 隐藏答案
+// 如果您想在一段时间后自动隐藏答案，可以添加一个定时器
 function hideAnswer() {
-    var answer = document.getElementById("answer");
-    answer.classList.add("hidden");
+    setTimeout(() => {
+        document.getElementById("answer").classList.add("hidden");
+    }, 5000); // 5秒后隐藏答案
 }
